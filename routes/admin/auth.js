3be0288ac88ff62be4e7ccad7e1b2f66c85a7473
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-06-25 15:25:32 -0600

    move existing email check to custom express validator function

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { check } = require("express-validator");
+const { check, validationResult } = require("express-validator");
 const usersRepo = require("../../repositories/users");
 const signupTemplate = require("../../views/admin/auth/signup");
 const signinTemplate = require("../../views/admin/auth/signin");
@@ -10,21 +10,40 @@ router.get("/signup", (req, res) => {
   res.send(signupTemplate({ req }));
 });
 
-router.post("/signup", [
-  check("email").isEmail().normalizeEmail(),
-  check("password"),
-  check("passwordConfirmation")
-], async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
-  const existingUser = await usersRepo.getOneBy({ email });
+router.post(
+  "/signup",
+  [
+    check("email")
+      .trim()
+      .normalizeEmail()
+      .isEmail()
+      .withMessage("Must be a valid email")
+      .custom(async email => {
+        const existingUser = await usersRepo.getOneBy({ email });
+        if (existingUser) throw new Error("Email in use");
+      }),
+    check("password")
+      .trim()
+      .isLength({ min: 8, max: 20 })
+      .withMessage("Must be between 8 and 20 characters"),
+    check("passwordConfirmation")
+      .trim()
+      .isLength({ min: 8, max: 20 })
+      .withMessage("Must be between 8 and 20 characters")
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
 
-  if (existingUser) return res.send("Email already in use");
-  if (password !== passwordConfirmation) return res.send("Passwords must match");
-  
-  const user = await usersRepo.create({ email, password });
-  req.session.userId = user.id;
-  res.send("Account Created!");
-});
+    const { email, password, passwordConfirmation } = req.body;
+    
+    if (password !== passwordConfirmation) return res.send("Passwords must match");
+    
+    const user = await usersRepo.create({ email, password });
+    req.session.userId = user.id;
+    res.send("Account Created!"); 
+  }
+);
 
 router.get("/signout", (req, res) => {
   req.session = null;
