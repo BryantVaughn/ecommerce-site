e46598e7b68b1c5ba9fe776d2babf8576e694dac
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-06-26 17:50:19 -0600

    product edit template

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -35,11 +35,15 @@ router.post(
   }
 );
 
-router.get("/admin/products/:id/edit", async (req, res) => {
+router.get("/admin/products/:id/edit", requireAuth, async (req, res) => {
   const product = await productsRepo.getOne(req.params.id);
   if (!product) return res.send("Product not found");
 
   res.send(productsEditTemplate({ product }));
+});
+
+router.post("/admin/products/:id/edit", requireAuth, async (req, res) => {
+  
 });
 
 module.exports = router;
