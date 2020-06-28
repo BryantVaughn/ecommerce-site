748ee08fc5acb5d1554fd4438d2cbb061d86c9ff
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-06-28 15:26:19 -0600

    display list of items in cart

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,8 @@
 const express = require("express");
+
 const cartsRepo = require("../repositories/carts");
+const productsRepo = require("../repositories/products");
+const cartShowTemplate = require("../views/carts/show");
 
 const router = express.Router();
 
@@ -21,6 +24,18 @@ router.post("/cart/products", async (req, res) => {
   });
 
   res.send("Item added to cart");
+});
+
+router.get("/cart", async (req, res) => {
+  if (!req.session.cartId) return res.redirect("/");
+  
+  const cart = await cartsRepo.getOne(req.session.cartId);
+  for (let item of cart.items) {
+    const product = await productsRepo.getOne(item.id);
+    item.product = product;
+  }
+
+  res.send(cartShowTemplate({ items: cart.items }));
 });
 
 module.exports = router;
