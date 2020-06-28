0031323bb5d666edb6a401228f094cbd95b7294c
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-06-28 16:16:39 -0600

    redirect to cart when item is added

--- routes/carts.js
+++ routes/carts.js
@@ -23,7 +23,7 @@ router.post("/cart/products", async (req, res) => {
     items: cart.items
   });
 
-  res.send("Item added to cart");
+  res.redirect("/cart");
 });
 
 router.get("/cart", async (req, res) => {
